bf5fb6864c1d3a63be5dc8afa4b580fb62a0e338
Robert Bragg <robert@sixbynine.org>
2015-06-16 09:39:39 +0100

    web: add emscripten gputop_web_ library

---
+++ gputop/gputop-web.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2015 Intel Corporation.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#pragma once
+
+#include <stdbool.h>
+
+void _gputop_web_console_log(const char *message);
+void gputop_web_console_log(const char *format, ...);
+
+void _gputop_web_console_info(const char *message);
+void gputop_web_console_info(const char *format, ...);
+
+void _gputop_web_console_warn(const char *message);
+void gputop_web_console_warn(const char *format, ...);
+
+void _gputop_web_console_error(const char *message);
+void gputop_web_console_error(const char *format, ...);
+
+void _gputop_web_console_assert(bool condition, const char *message) __attribute__((noreturn));
+void gputop_web_console_assert(bool condition, const char *format, ...) __attribute__((noreturn));
+
+void _gputop_web_console_trace(void);
+
+void _gputop_web_worker_post(const char *message);
---
+++ gputop/web/gputop-web-lib.js
@@ -0,0 +1,39 @@
+"use strict";
+
+var LibraryGpuTopWeb = {
+    //$GpuTopWeb__deps: ['$Browser'],
+    $GpuTopWeb: {
+
+    },
+
+    _gputop_web_console_log: function (message) {
+        console.log(Pointer_stringify(message));
+    },
+
+    _gputop_web_console_info: function (message) {
+        console.info(Pointer_stringify(message));
+    },
+
+    _gputop_web_console_warn: function (message) {
+        console.warn(Pointer_stringify(message));
+    },
+
+    _gputop_web_console_error: function (message) {
+        console.error(Pointer_stringify(message));
+    },
+
+    _gputop_web_console_assert: function (condition, message) {
+        console.assert(condition, Pointer_stringify(message));
+    },
+
+    _gputop_web_console_trace: function () {
+        console.trace();
+    },
+
+    _gputop_web_worker_post: function (message) {
+	postMessage(Pointer_stringify(message));
+    },
+};
+
+autoAddDeps(LibraryGpuTopWeb, '$GpuTopWeb');
+mergeInto(LibraryManager.library, LibraryGpuTopWeb);
