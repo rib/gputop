58d4db26341d92e711add34545762b5c4dd55731
Adrian Moldovan <adrian.moldovan@intel.com>
2015-12-11 17:49:12 +0000

    gputop-perf: Added functions needed to generate fake data.
    Implemented functions that deal with the fake data generation
    that works both with ncurses and --remote.
    
    Conflicts:
    	gputop/gputop-perf.c

--- gputop/gputop-perf.h
+++ gputop/gputop-perf.h
@@ -29,6 +29,7 @@
 
 #ifndef EMSCRIPTEN
 #include <uv.h>
+#include <time.h>
 #endif
 
 #include "i915_oa_drm.h"
@@ -63,7 +64,7 @@ struct gputop_devinfo {
 };
 
 struct gputop_perf_query;
-
+uint64_t get_time();
 struct gputop_perf_query_counter
 {
    const char *name;
@@ -241,6 +242,14 @@ struct gputop_perf_stream
 
     int fd;
     uv_poll_t fd_poll;
+    uv_timer_t fd_timer;
+
+// fields used for fake data:
+    uint64_t start_time;  // stream opening time
+    uint32_t gen_so_far; // amount of reports generated since stream opening
+    uint32_t prev_clocks; // the previous value of clock ticks
+    uint32_t period; // the period in nanoseconds calculated from exponent
+    uint32_t prev_timestamp; // the previous timestamp value
 
     /* XXX: reserved for whoever opens the stream */
     struct {
@@ -265,6 +274,8 @@ bool gputop_perf_initialize(void);
 bool gputop_i915_perf_oa_overview_open(int metric_set, bool enable_per_ctx);
 void gputop_i915_perf_oa_overview_close(void);
 
+int fake_read(struct gputop_perf_stream *stream, uint8_t *buf, int buf_length);
+
 void gputop_perf_accumulator_clear(struct gputop_perf_stream *stream);
 void gputop_perf_accumulate(struct gputop_perf_stream *stream,
 			    const uint8_t *report0,
@@ -285,6 +296,7 @@ extern bool gputop_perf_trace_empty;
 extern bool gputop_perf_trace_full;
 extern uint8_t *gputop_perf_trace_head;
 extern int gputop_perf_n_samples;
+extern bool gputop_fake_mode;
 
 uint64_t read_report_timestamp(const uint32_t *report);
 uint64_t read_uint64_oa_counter(const struct gputop_perf_query *query,
@@ -310,6 +322,7 @@ gputop_open_i915_perf_oa_query(struct gputop_perf_query *query,
 			       struct ctx_handle *ctx,
 			       size_t perf_buffer_size,
 			       void (*ready_cb)(uv_poll_t *poll, int status, int events),
+                               void (*ready_cb_fake)(uv_timer_t *poll),
 			       bool overwrite,
 			       char **error);
 struct gputop_perf_stream *
