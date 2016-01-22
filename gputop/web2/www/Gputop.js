fe70f43ccfe5a553c153ae2d9fbfb799b8e9bb9b
Sergio Martinez <sergio.martinez.rodriguez@intel.com>
2016-01-22 16:36:11 +0000

    gputop-ui: Separating MVC to allow nodejs text interface
    Adding support for guids into the web2 ui

--- gputop/web2/www/Gputop.js
+++ gputop/web2/www/Gputop.js
@@ -1,4 +1,14 @@
+//@ sourceURL=Gputop.js
+
 // https://google.github.io/styleguide/javascriptguide.xml
+
+Object.size = function(obj) {
+    var size = 0, key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) size++;
+    }
+    return size;
+};
 
 //------------------------------ Protobuffer Init ----------------------------------------
 
@@ -10,26 +20,41 @@ var ProtoBuf = dcodeIO.ProtoBuf;
 
 var proto_builder = ProtoBuf.loadProtoFile("./proto/gputop.proto");
 
+//------------------------------ METRIC ---------------------------------------------------
+function Counter () {
+    this.idx_ = -1;    
+    this.emscripten_idx_ = -1;
+    this.available_ = false;
+    this.xml_ = "<xml/>";
+}
+
+//------------------------------ METRIC ---------------------------------------------------
+function Metric () {
+    this.guid_ = "undefined";
+    this.xml_ = "<xml/>";
+    this.supported_ = false;
+    this.counters_ = {};
+    this.metric_set_ = 0;
+}
+
+Metric.prototype.print = function() {
+    gputop_ui.syslog(this.guid_);
+}
+
 //------------------------------ GPUTOP ---------------------------------------------------
 function Gputop () {
-
+    this.map_metrics_ = {};
+   
     this.is_connected_ = false;
     // Gputop generic configuration
     this.config_ = {
         url_path: 'localhost',
         uri_port: 7890,
-        architecture: 'skl'
+        architecture: 'ukn'
     }
-
 
     // Initialize protobuffers
     this.builder_ = proto_builder.build("gputop");
-
-    // Array initializer for tabs
-    this.tabs_ = [
-    'Test tab 1',
-    'Test tab 2'
-    ];
 
     // Queries
     this.all_oa_queries_ = [];
@@ -40,24 +65,79 @@ function Gputop () {
     this.query_handles_ = [];
 }
 
-Gputop.prototype.load_oa_queries = function(architecture) {
-    console.log(" Loading " + architecture);
-    this.config_.architecture = architecture;
+Gputop.prototype.get_metrics_xml = function() {
+    return this.metrics_xml_;
+}
 
-    $( '#pane2' ).load( "ajax/Panel2.html", function() {
-        console.log('Panel2 load)');
-        //timeout();
+Gputop.prototype.get_map_metric = function(guid){
+    var metric;    
+    if (guid in this.map_metrics_) {
+        metric = this.map_metrics_[guid];
+    } else {
+        metric = new Metric();
+        metric.guid_ = guid;        
+        metric.metric_set_ = Object.keys(this.map_metrics_[guid]).length;
+        this.map_metrics_[guid] = metric;
+    }
+    return metric;
+}
+
+function gputop_read_metrics_set() {
+    try { 
+        var $set = $(this);
+        var title = $set.attr("name");
+        var guid = $set.attr("guid");
+    
+        gputop_ui.syslog('Found metric ' + title);
+    
+        var metric = gputop.get_map_metric(guid);    
+        metric.xml_ = $set;
+        metric.print();  
+        gputop.map_metrics_[guid] = metric;
+    } catch (e) {
+        gputop_ui.syslog("Catch parsing " + e);
+    }
+} // read_metrics_set
+
+Gputop.prototype.load_xml_metrics = function(xml) {
+    this.metrics_xml_ = xml;
+    $(xml).find("set").each(gputop_read_metrics_set);
+
+    gputop_ui.load_metrics_panel(function() {
+        //TODO(@sergioamr) How do you get the first entry in JS?
+        for(var guid in gputop.map_metrics_) {
+            gputop.open_oa_query_for_trace(guid);
+            return;
+        }
     });
 }
 
-Gputop.prototype.open_oa_query_for_trace = function(idx)
-{
-    var oa_query = this.all_oa_queries_[idx];
+Gputop.prototype.load_oa_queries = function(architecture) {    
+    console.log("Loading " + architecture);
+    this.config_.architecture = architecture;
 
-    if (typeof oa_query === 'undefined') {
-        oa_query = new this.builder_.OAQueryInfo();
-        oa_query.metric_set = 1;    /* 3D test */
+    // read counters from xml file and populate the website
+    var xml_name = "xml/oa-"+ architecture +".xml";
+    $.get(xml_name, this.load_xml_metrics);
+}
+
+Gputop.prototype.open_oa_query_for_trace = function(guid) {
+    if (guid == undefined) {
+        gputop_ui.show_alert("GUID missing while trying to opening query","alert-danger");
+        return;
     }
+    
+    gputop_ui.syslog("Launch query GUID " + guid);
+    var metric = this.get_map_metric(guid);
+
+    if (metric.supported_ == false) {
+        gputop_ui.show_alert("Metric "+guid+" not supported","alert-danger");        
+        return;
+    }
+    
+    var oa_query = new this.builder_.OAQueryInfo();
+    oa_query.guid = guid;
+    oa_query.metric_set = metric.metric_set_;    /* 3D test */    
 
     /* The timestamp for HSW+ increments every 80ns
      *
@@ -81,7 +161,7 @@ Gputop.prototype.open_oa_query_for_trace = function(idx)
 
     oa_query.period_exponent = 16 ;
 
-    open.id = 1; //[TODO @sergioam] oa_query ID
+    open.id = this.query_id_; // oa_query ID
     open.overwrite = false;   /* don't overwrite old samples */
     open.live_updates = true; /* send live updates */
                          /* nanoseconds of aggregation
@@ -106,7 +186,7 @@ Gputop.prototype.open_oa_query_for_trace = function(idx)
     msg.encode();
     this.socket_.send(msg.toArrayBuffer());
 
-    log.value += "Sent: Request "+msg.uuid+"\n";
+    gputop_ui.syslog("Sent: Request "+msg.uuid);
 
     this.query_handles_.push(this.query_id);
 
@@ -133,8 +213,8 @@ Gputop.prototype.generate_uuid = function()
      * http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
      */
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-	var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-	return v.toString(16);
+    	var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+    	return v.toString(16);
     });
 }
 
@@ -147,29 +227,27 @@ Gputop.prototype.request_features = function() {
 
         msg.encode();
         this.socket_.send(msg.toArrayBuffer());
-        log.value += "Sent: Request "+msg.uuid+"\n";
+        gputop_ui.syslog("Sent: Request "+msg.uuid);
     } else {
-        log.value += "Not connected\n";
+        gputop_ui.syslog("Not connected");
     }
 }
 
-Gputop.prototype.display_features = function(features) {
-    $( "#gputop-connecting" ).hide();
-    $( "#gputop-cpu" ).append( features.get_cpu_model() );
-    $( "#gputop-kernel-build" ).append( features.get_kernel_build() );
-    $( "#gputop-kernel-release" ).append( features.get_kernel_release() );
-    $( "#gputop-n-cpus" ).append( features.get_n_cpus() );
-    $( "#gputop-kernel-performance-query" ).append( features.get_has_gl_performance_query() );
-    $( "#gputop-kernel-performance-i915-oa" ).append( features.get_has_i915_oa() );
+Gputop.prototype.metric_supported = function(element, index, array){    
+    var metric = gputop.get_map_metric(element);
+    metric.supported_ = true; 
+    metric.print();
 }
 
-// types of alerts: alert-success alert-info alert-warning alert-danger
-Gputop.prototype.show_alert = function(message,alerttype){
-    $('#alert_placeholder').append('<div id="alertdiv" class="alert ' +
-        alerttype + '"><a class="close" data-dismiss="alert">×</a><span>'+message+'</span></div>')
-        setTimeout(function() { // this will automatically close the alert and remove this if the users doesnt close it in 5 secs
-            $("#alertdiv").remove();
-        }, 5000);
+Gputop.prototype.process_features = function(features){
+    gputop_ui.display_features(features);
+
+    features.supported_oa_query_guids.forEach(this.metric_supported);
+
+    var di = features.devinfo;   
+    _update_features(di.devid, di.n_eus,  di.n_eu_slices,
+        di.n_eu_sub_slices, di.eu_threads_count, di.subslice_mask,
+        di.slice_mask);
 }
 
 Gputop.prototype.get_socket = function(websocket_url) {
@@ -177,21 +255,20 @@ Gputop.prototype.get_socket = function(websocket_url) {
     socket.binaryType = "arraybuffer"; // We are talking binary
 
     socket.onopen = function() {
-        log.value += "Connected\n";
-        gputop.show_alert("Succesfully connected to GPUTOP","alert-info");
+        gputop_ui.syslog("Connected");
+        gputop_ui.show_alert("Succesfully connected to GPUTOP","alert-info");
         gputop.request_features();
         gputop.is_connected_ = true;
-
     };
 
     socket.onclose = function() {
-        log.value += "Disconnected\n";
-        gputop.is_connected_ = false;
-        gputop.show_alert("Failed connecting to GPUTOP <p\>Retry in 5 seconds","alert-danger");
+        gputop_ui.syslog("Disconnected");                       
+        gputop_ui.show_alert("Failed connecting to GPUTOP <p\>Retry in 5 seconds","alert-danger");
         setTimeout(function() { // this will automatically close the alert and remove this if the users doesnt close it in 5 secs
-            log.value += "Connection retry\n";
             gputop.connect();
         }, 5000);
+        
+        gputop.is_connected_ = false;
     };
 
     socket.onmessage = function(evt) {
@@ -208,30 +285,15 @@ Gputop.prototype.get_socket = function(websocket_url) {
                 case 2: /* WS_MESSAGE_PROTOBUF */
                     var msg = gputop.builder_.Message.decode(data);
                     if (msg.features != undefined) {
-                        log.value += "Features: "+msg.features.get_cpu_model()+"";
-                        gputop.display_features(msg.features);
-                        gputop.open_oa_query_for_trace(this.query_id);
-
-                        var di = msg.features.devinfo;
-                        _update_features(di.devid, di.n_eus,  di.n_eu_slices,
-                            di.n_eu_sub_slices, di.eu_threads_count, di.subslice_mask,
-                            di.slice_mask);
+                        gputop_ui.syslog("Features: "+msg.features.get_cpu_model());
+                        gputop.process_features(msg.features);
                     }
                     if (msg.log != undefined) {
                         var entries = msg.log.entries;
                         entries.forEach(function(entry) {
-                        	var color = "red";
-                        	switch(entry.log_level) {
-                        		case 0: color = "orange"; break;
-                        		case 1: color = "green"; break;
-                        		case 2: color = "yellow"; break;
-                        		case 3: color = "blue"; break;
-                        		case 4: color = "black"; break;
-                        	}
-                        	$('#editor').append("<font color='"+color+"'>"+entry.log_message+"<br/></font>");
+                            gputop_ui.log(entry.log_level, entry.log_message);
                         });
                     }
-
 
                 break;
                 case 3: /* WS_MESSAGE_I915_PERF */
@@ -245,6 +307,7 @@ Gputop.prototype.get_socket = function(websocket_url) {
             }
         } catch (err) {
             log.value += "Error: "+err+"\n";
+            console.log("Error: "+err);
         }
     };
 
@@ -254,34 +317,12 @@ Gputop.prototype.get_socket = function(websocket_url) {
 // Connect to the socket for transactions
 Gputop.prototype.connect = function() {
     var websocket_url = this.get_websocket_url();
-    console.log('Connecting to port ' + websocket_url);
+    gputop_ui.syslog('Connecting to port ' + websocket_url);
     //----------------- Data transactions ----------------------
     this.socket_ = this.get_socket(websocket_url);
 }
 
-Gputop.prototype.init = function() {
-    console.log(' Gputop Init ');
-    $( "#gputop-overview-panel" ).load( "ajax/overview.html", function() {
-        console.log('gputop-overview-panel load');
-        gputop.connect();
-    });
-}
-
 Gputop.prototype.dispose = function() {
-  this.tabs_ = null;
 };
 
 var gputop = new Gputop();
-
-// jquery code
-$( document ).ready(function() {
-    //log = $( "#log" );
-    log = document.getElementById("log");
-    $( "#gputop-entries" ).append( '<li><a href="#">Test</a></li>' );
-    gputop.init();
-    $('#editor').wysiwyg();
-});
-
-function allReady() {
-    console.log(" All ready for emscripten ");
-}
---
+++ gputop/web2/www/Gputop_ui.js
@@ -0,0 +1,69 @@
+//@ sourceURL=Gputop_ui.js
+
+function Gputop_ui () {
+
+}
+
+Gputop_ui.prototype.metric_not_supported = function(metric) {    
+    alert(" Metric not supported " + metric.title_)
+}
+
+Gputop_ui.prototype.display_features = function(features) {
+    $( "#gputop-connecting" ).hide();
+    $( "#gputop-cpu" ).append( features.get_cpu_model() );
+    $( "#gputop-kernel-build" ).append( features.get_kernel_build() );
+    $( "#gputop-kernel-release" ).append( features.get_kernel_release() );
+    $( "#gputop-n-cpus" ).append( features.get_n_cpus() );
+    $( "#gputop-kernel-performance-query" ).append( features.get_has_gl_performance_query() );
+    $( "#gputop-kernel-performance-i915-oa" ).append( features.get_has_i915_oa() );
+}
+
+// types of alerts: alert-success alert-info alert-warning alert-danger
+Gputop_ui.prototype.show_alert = function(message,alerttype){
+    $('#alert_placeholder').append('<div id="alertdiv" class="alert ' +
+        alerttype + '"><a class="close" data-dismiss="alert">×</a><span>'+message+'</span></div>')
+        setTimeout(function() { // this will automatically close the alert and remove this if the users doesnt close it in 5 secs
+            $("#alertdiv").remove();
+        }, 5000);
+}
+
+Gputop_ui.prototype.log = function(log_level, log_message){
+    var color = "red";
+    switch(log_level) {
+        case 0: color = "orange"; break;
+        case 1: color = "green"; break;
+        case 2: color = "yellow"; break;
+        case 3: color = "blue"; break;
+        case 4: color = "black"; break;
+    }    
+    $('#editor').append("<font color='"+color+"'>"+log_message+"<br/></font>");
+}
+
+Gputop_ui.prototype.syslog = function(message){
+    log.value += message + "\n";
+}
+
+Gputop_ui.prototype.init_interface = function(){
+    $( "#gputop-overview-panel" ).load( "ajax/overview.html", function() {
+        console.log('gputop-overview-panel load');
+        gputop.connect();
+    });
+}
+
+Gputop_ui.prototype.load_metrics_panel = function(callback_success) {
+    $( '#pane2' ).load( "ajax/metrics.html", function() {
+        console.log('Metrics panel loaded');
+        callback_success();        
+    });
+}
+
+var gputop_ui = new Gputop_ui();
+
+// jquery code
+$( document ).ready(function() {
+    //log = $( "#log" );
+    log = document.getElementById("log");
+    $( "#gputop-entries" ).append( '<li><a href="#">Test</a></li>' );
+    gputop_ui.init_interface();
+    $('#editor').wysiwyg();
+});
