bbda5aff7939f13504b8f50ea454f821830ef821
Robert Bragg <robert.bragg@intel.com>
2016-03-22 12:50:07 +0000

    travis: more brown paper bag fixes

--- scripts/deploy-demo-site.sh
+++ scripts/deploy-demo-site.sh
@@ -4,11 +4,11 @@ mkdir deploy
 cd deploy
 git config --global user.email "Travis@travis.org"
 git config --global user.name "Travis CI"
-git clone --quiet https://${GH_TOKEN}@github.com/gputop/gputop.github.io.git > /dev/null
+git clone --quiet https://${GH_TOKEN}@github.com/gputop/gputop.github.io.git &> /dev/null
 cd gputop.github.io
 rm -rf *
 cp -r $TRAVIS_BUILD_DIR/install/share/remote/* .
 git add --all
 git status
 git commit -m "Deployed by Travis"
-git push origin master
+git push origin master &> /dev/null
