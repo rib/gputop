2fd280614ff99eb1582aace7794595c16f8d6435
Robert Bragg <robert@sixbynine.org>
2016-08-03 18:04:39 +0100

    Add comments clarifying some devinfo subslice mask details

--- gputop-server/gputop-perf.c
+++ gputop-server/gputop-perf.c
@@ -732,6 +732,14 @@ init_dev_info(int drm_fd, uint32_t devid)
                 gputop_devinfo.gen = 8;
             } else if (IS_SKYLAKE(devid)) {
                 s_max = 3;
+
+                /* XXX: beware that the kernel (as of writing) actually works
+                 * as if ss_max == 4 since the HW register that reports the
+                 * global subslice mask has 4 bits while in practice the limit
+                 * is 3. It's also important that we initialize $SubsliceMask
+                 * with 3 bits per slice since that's what the counter
+                 * availability expressions in XML expect.
+                 */
                 ss_max = 3;
                 gputop_devinfo.gen = 9;
 
@@ -760,10 +768,13 @@ init_dev_info(int drm_fd, uint32_t devid)
             gputop_devinfo.n_eu_slices = __builtin_popcount(slice_mask);
             gputop_devinfo.slice_mask = slice_mask;
 
-            /* Note: some of the metrics we have (as described in XML)
-             * are conditional on a $SubsliceMask variable which is
-             * expected to also reflect the slice mask by packing
-             * together subslice masks for each slice in one value...
+            /* Note: the _SUBSLICE_MASK param only reports a global subslice
+             * mask which applies to all slices.
+             *
+             * Note: some of the metrics we have (as described in XML) are
+             * conditional on a $SubsliceMask variable which is expected to
+             * also reflect the slice mask by packing together subslice masks
+             * for each slice in one value..
              */
             for (s = 0; s < s_max; s++) {
                 if (slice_mask & (1<<s)) {
