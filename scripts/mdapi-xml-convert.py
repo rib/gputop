c105b17e013f533994a1c5c4fcdc87ddb53e7932
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2017-04-07 15:56:52 +0100

    gputop-data: add configs for Kabylake GT2/3

--- scripts/mdapi-xml-convert.py
+++ scripts/mdapi-xml-convert.py
@@ -152,6 +152,24 @@ chipsets = {
                 0x2364, # OACTXID
                 },
             },
+         'KBLGT2': {
+            'a_offset': 16,
+            'b_offset': 192,
+            'c_offset': 224,
+            'config_reg_blacklist': {
+                0x9840, # GDT_CHICKEN_BITS/NOA_ENABLE
+                0x2364, # OACTXID
+                },
+            },
+         'KBLGT3': {
+            'a_offset': 16,
+            'b_offset': 192,
+            'c_offset': 224,
+            'config_reg_blacklist': {
+                0x9840, # GDT_CHICKEN_BITS/NOA_ENABLE
+                0x2364, # OACTXID
+                },
+            },
         }
 
 register_types = { 'OA', 'NOA', 'FLEX', 'PM' }
@@ -975,4 +993,3 @@ for set in metrics.findall(".//set"):
         print("    </register_config>")
     print("  </set>\n")
 print("</metrics>")
-
--- scripts/update-guids.py
+++ scripts/update-guids.py
@@ -167,7 +167,7 @@ for arg in args.xml:
 
 
 
-chipsets = [ 'hsw', 'bdw', 'chv', 'sklgt2', 'sklgt3', 'sklgt4', 'bxt' ]
+chipsets = [ 'hsw', 'bdw', 'chv', 'sklgt2', 'sklgt3', 'sklgt4', 'bxt', 'kblgt2', 'kblgt3' ]
 
 for chipset in chipsets:
     public = et.parse('oa-' + chipset + '.xml')
