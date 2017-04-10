dc8524dabdcddf07e60b1109882590c1fd82bbb8
Robert Bragg <robert.bragg@intel.com>
2017-04-10 20:28:38 +0100

    update deploy-demo-site.sh

--- scripts/deploy-demo-site.sh
+++ scripts/deploy-demo-site.sh
@@ -7,7 +7,7 @@ git config --global user.name "Travis CI"
 git clone --quiet https://${GH_TOKEN}@github.com/gputop/gputop.github.io.git &> /dev/null
 cd gputop.github.io
 rm -rf *
-cp -r $TRAVIS_BUILD_DIR/install/share/remote/* .
+cp -r $TRAVIS_BUILD_DIR/gputop-travis-build/share/remote/* .
 echo "disable jekyll">.nojekyll
 echo "www.gputop.com" >> CNAME
 git add --all
