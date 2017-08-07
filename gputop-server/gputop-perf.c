rib/gputop

commit c964af987f49706a666141d00e363fd0ccae62c9

server: rework sysfs read function to return errors

gputop-server/gputop-perf.c
@@ -135,14 +135,14 @@ static gputop_list_t ctx_handles_list = {
 
 /******************************************************************************/
 
-static uint64_t
-sysfs_card_read(const char *file)
+static bool
+sysfs_card_read(const char *file, uint64_t *value)
 {
-        char buf[512];
+    char buf[512];
 
-        snprintf(buf, sizeof(buf), "/sys/class/drm/card%d/%s", drm_card, file);
+    snprintf(buf, sizeof(buf), "/sys/class/drm/card%d/%s", drm_card, file);
 
-        return gputop_read_file_uint64(buf);
+    return gputop_read_file_uint64(buf, value);
 }
 
 
@@ -769,8 +769,12 @@ init_dev_info(int drm_fd, uint32_t devid, const struct gen_device_info *devinfo)
         }
 
         assert(drm_card >= 0);
-        gputop_devinfo.gt_min_freq = sysfs_card_read("gt_min_freq_mhz") * 1000000;
-        gputop_devinfo.gt_max_freq = sysfs_card_read("gt_max_freq_mhz") * 1000000;
+        if (!sysfs_card_read("gt_min_freq_mhz", &gputop_devinfo.gt_min_freq))
+            fprintf(stderr, "Unable to read GT min frequency\n");
+        if (!sysfs_card_read("gt_max_freq_mhz", &gputop_devinfo.gt_max_freq))
+            fprintf(stderr, "Unable to read GT max frequency\n");
+        gputop_devinfo.gt_min_freq *= 1000000;
+        gputop_devinfo.gt_max_freq *= 1000000;
     }
 
     gputop_devinfo.eu_threads_count = gputop_devinfo.n_eus * devinfo->num_thread_per_eu;
@@ -1356,14 +1360,15 @@ read_device_param(const char *stem, int id, const char *param)
 {
     char *name;
     int ret = asprintf(&name, "/sys/class/drm/%s%u/device/%s", stem, id, param);
-    uint32_t value;
+    uint64_t value;
+    bool success;
 
     assert(ret != -1);
 
-    value = gputop_read_file_uint64(name);
+    success = gputop_read_file_uint64(name, &value);
     free(name);
 
-    return value;
+    return success ? value : 0;
 }
 
 static int
@@ -1439,8 +1444,8 @@ gputop_enumerate_metrics_via_sysfs(void)
                  "/sys/class/drm/card%d/metrics/%s/id",
                  drm_card, entry->d_name);
 
-        metric_set->perf_oa_metrics_set = gputop_read_file_uint64(buffer);
-        array_append(gputop_perf_oa_supported_metric_set_uuids, &metric_set->hw_config_guid);
+        if (gputop_read_file_uint64(buffer, &metric_set->perf_oa_metrics_set))
+          array_append(gputop_perf_oa_supported_metric_set_uuids, &metric_set->hw_config_guid);
     }
     closedir(metrics_dir);
 

gputop-server/gputop-debugfs.c
@@ -121,6 +121,7 @@ void *gputop_debugfs_read(const char *filename, int *len)
 uint64_t gputop_debugfs_read_uint64(const char *filename)
 {
     char buf[1024];
+    uint64_t value;
 
     if (!debugfs_mount()) {
         return 0;
@@ -128,7 +129,10 @@ uint64_t gputop_debugfs_read_uint64(const char *filename)
 
     snprintf(buf, sizeof(buf), "%s/%s", debugfs_path, filename);
 
-    return gputop_read_file_uint64(buf);
+    if (!gputop_read_file_uint64(buf, &value))
+	return 0;
+
+    return value;
 }
 
 char **

gputop-server/gputop-oa-metrics.h
@@ -117,7 +117,7 @@ struct gputop_metric_set
     struct gputop_metric_set_counter *counters;
     int n_counters;
 
-    int perf_oa_metrics_set;
+    uint64_t perf_oa_metrics_set;
     int perf_oa_format;
     int perf_raw_size;
 

gputop-server/gputop-util.h
@@ -152,22 +152,24 @@ gputop_get_time(void);
 bool
 gputop_read_file(const char *filename, void *buf, int max);
 
-static uint64_t __attribute__((unused))
-gputop_read_file_uint64(const char *file)
+static bool __attribute__((unused))
+gputop_read_file_uint64(const char *file, uint64_t *value)
 {
     char buf[32];
     int fd, n;
 
     fd = open(file, 0);
     if (fd < 0)
-        return 0;
+        return false;
     n = read(fd, buf, sizeof (buf) - 1);
     close(fd);
     if (n < 0)
-        return 0;
+        return false;
 
     buf[n] = '\0';
-    return strtoull(buf, 0, 0);
+    *value = strtoull(buf, 0, 0);
+
+    return true;
 }
 
 

commit bc3c67862a51adb1bc873c455dd27178c16a5d7d

server: fix loadable configs
Fixes: c964af987f ("server: rework sysfs read function to return errors")

gputop-server/gputop-perf.c
@@ -1444,8 +1444,10 @@ gputop_enumerate_metrics_via_sysfs(void)
                  "/sys/class/drm/card%d/metrics/%s/id",
                  drm_card, entry->d_name);
 
-        if (gputop_read_file_uint64(buffer, &metric_set->perf_oa_metrics_set))
-          array_append(gputop_perf_oa_supported_metric_set_uuids, &metric_set->hw_config_guid);
+        if (sysfs_card_read(buffer, &metric_set->perf_oa_metrics_set)) {
+	    array_append(gputop_perf_oa_supported_metric_set_uuids,
+			 &metric_set->hw_config_guid);
+	}
     }
     closedir(metrics_dir);
 