05f16632c0fdfc53248897d1f78a110ca1c4d2a4
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2018-01-30 14:14:24 +0000

    deploy: fix site deployment

--- scripts/deploy-demo-site.sh
+++ scripts/deploy-demo-site.sh
@@ -7,14 +7,14 @@ git config --global user.name "Travis CI"
 git clone --quiet https://${GH_TOKEN}@github.com/gputop/gputop.github.io.git &> /dev/null
 cd gputop.github.io
 rm -rf *
-cp ${TRAVIS_BUILD_DIR}/travis-build/gputop-ui/*.js ${TRAVIS_BUILD_DIR}/travis-build/gputop-ui/*.wasm .
-if [ -d ${TRAVIS_BUILD_DIR}/travis-build/gputop-ui/gputop-ui.wasm.map ]; then
-    cp ${TRAVIS_BUILD_DIR}/travis-build/gputop-ui/gputop-ui.wasm.map .
+cp ${TRAVIS_BUILD_DIR}/travis-build/ui/*.js ${TRAVIS_BUILD_DIR}/travis-build/ui/*.wasm .
+if [ -d ${TRAVIS_BUILD_DIR}/travis-build/ui/gputop-ui.wasm.map ]; then
+    cp ${TRAVIS_BUILD_DIR}/travis-build/ui/gputop-ui.wasm.map .
 fi
-if [ -d ${TRAVIS_BUILD_DIR}/travis-build/gputop-ui/gputop-ui.wast ]; then
-    cp ${TRAVIS_BUILD_DIR}/travis-build/gputop-ui/gputop-ui.wast .
+if [ -d ${TRAVIS_BUILD_DIR}/travis-build/ui/gputop-ui.wast ]; then
+    cp ${TRAVIS_BUILD_DIR}/travis-build/ui/gputop-ui.wast .
 fi
-cp ${TRAVIS_BUILD_DIR}/gputop-ui/*.html ${TRAVIS_BUILD_DIR}/gputop-ui/*.css ${TRAVIS_BUILD_DIR}/gputop-ui/favicon.ico .
+cp ${TRAVIS_BUILD_DIR}/ui/*.html ${TRAVIS_BUILD_DIR}/ui/*.css ${TRAVIS_BUILD_DIR}/ui/favicon.ico .
 echo "disable jekyll">.nojekyll
 echo "www.gputop.com" >> CNAME
 git add --all
